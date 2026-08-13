568408453c38244d605f67f551b96f057ebe16d3
Siva Sankar <attilisiva459@gmail.com>
2026-08-13 18:55:38 +0530

    added refersh token configuration

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -7,4 +7,20 @@ const api = axios.create({
   },
 });
 
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
